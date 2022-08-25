db0151ed58f29accbc632262bc9f67a6232b8311
Alexander Schilling Miranda <alexander.schilling.miranda@gmail.com>
2022-08-25 15:38:25 -0400

    refactor: fix ESLint problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { FunctionComponent } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+const App: FunctionComponent = () => {
   return (
     <div className="App">
       <header className="App-header">
