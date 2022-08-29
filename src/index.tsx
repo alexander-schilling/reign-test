963845a3cea46f6a47dc6097a1aee6e8f701c1b0
Alexander Schilling Miranda <alexander.schilling.miranda@gmail.com>
2022-08-29 14:31:45 -0400

    feat: remove React strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <AppRoutes />
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
