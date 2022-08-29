10999ed35834afc3c02109811c2883f547c47b3c
Alexander Schilling Miranda <alexander.schilling.miranda@gmail.com>
2022-08-29 14:48:49 -0400

    feat(pages/NewsPage): add FilterSelectComponent

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,7 +1,22 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
+import { NewsType } from '@/interfaces/NewsInterfaces';
+import { FilterSelectComponent } from '@/components/FilterSelectComponent';
 
 const NewsPage: FunctionComponent = () => {
-  return <div>NewsPage</div>;
+  const [currentFilter, setCurrentFilter] = useState<NewsType | undefined>(
+    undefined,
+  );
+  const [currentPage, setCurrentPage] = useState(0);
+
+  return (
+    <div>
+      <FilterSelectComponent
+        currentFilter={currentFilter}
+        updateCurrentFilter={setCurrentFilter}
+        updateCurrentPage={setCurrentPage}
+      />
+    </div>
+  );
 };
 
 export { NewsPage };
