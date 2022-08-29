de23079c26f93b776bbdccf883dd2696fc3f2ce1
Alexander Schilling Miranda <alexander.schilling.miranda@gmail.com>
2022-08-29 14:49:37 -0400

    feat(pages/NewsPage): add PostsContainer

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,12 +1,35 @@
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useCallback, useRef, useState } from 'react';
 import { NewsType } from '@/interfaces/NewsInterfaces';
+import { PostsContainer } from '@/components/PostsContainer';
 import { FilterSelectComponent } from '@/components/FilterSelectComponent';
+import useFavoritePosts from '@/hooks/useFavoritePosts';
+import usePostNews from '@/hooks/usePostNews';
 
 const NewsPage: FunctionComponent = () => {
   const [currentFilter, setCurrentFilter] = useState<NewsType | undefined>(
     undefined,
   );
   const [currentPage, setCurrentPage] = useState(0);
+  const { isLoading, posts, hasMore } = usePostNews(currentFilter, currentPage);
+  const { favoritePosts, addToFavorite, removeFromFavorite } =
+    useFavoritePosts();
+
+  const observer = useRef<IntersectionObserver>();
+  const lastPostElementRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (isLoading) return;
+      if (observer.current !== undefined) observer.current.disconnect();
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setCurrentPage((prevPage) => prevPage + 1);
+        }
+      });
+
+      if (node !== null) observer.current.observe(node);
+    },
+    [isLoading, hasMore],
+  );
 
   return (
     <div>
@@ -14,6 +37,13 @@ const NewsPage: FunctionComponent = () => {
         currentFilter={currentFilter}
         updateCurrentFilter={setCurrentFilter}
         updateCurrentPage={setCurrentPage}
+      />
+      <PostsContainer
+        posts={posts}
+        favoritePosts={favoritePosts}
+        addToFavorite={addToFavorite}
+        removeFromFavorite={removeFromFavorite}
+        lastPostElementRef={lastPostElementRef}
       />
     </div>
   );
